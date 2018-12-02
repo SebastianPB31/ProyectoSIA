4389c7d6ca483b5f6b0181afc1cfe8390c4be7a7
SebastianPB31 <sebastian.pb31@gmail.com>
2018-12-02 14:50:57 -0300

    Add files via upload

---
+++ ProyectoSIA/mainwindow.cpp
@@ -0,0 +1,213 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    model = new QSqlQueryModel;
+}
+
+MainWindow::~MainWindow()
+{
+    model->destroyed();
+    db.close();
+    delete ui;
+}
+
+void MainWindow::on_infoEmpleado_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+    ui->label->setText("INFORMACION DE EMPLEADOS");
+    // SQL query
+    // imprimir a pantalla
+    model->setQuery("SELECT * FROM EMPLEADO", db);
+    model->setHeaderData(0, Qt::Horizontal, QObject::tr("RUT"));
+    model->setHeaderData(1, Qt::Horizontal, QObject::tr("Nombre"));
+    model->setHeaderData(2, Qt::Horizontal, QObject::tr("Apellido"));
+    model->setHeaderData(3, Qt::Horizontal, QObject::tr("Sueldo"));
+    model->setHeaderData(4, Qt::Horizontal, QObject::tr("Telefono"));
+    model->setHeaderData(5, Qt::Horizontal, QObject::tr("Direccion"));
+    ui->tablaEmpleado->setModel(model);
+    ui->tablaEmpleado->show();
+}
+
+
+void MainWindow::on_listaProducto_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+    ui->label->setText("LISTA DE PRODUCTOS");
+    // SQL query
+    // imprimir a pantalla
+    model->setQuery("SELECT * FROM PRODUCTO", db);
+    model->setHeaderData(0, Qt::Horizontal, QObject::tr("Codigo"));
+    model->setHeaderData(1, Qt::Horizontal, QObject::tr("Codigo caja"));
+    model->setHeaderData(2, Qt::Horizontal, QObject::tr("Nombre producto"));
+    model->setHeaderData(3, Qt::Horizontal, QObject::tr("Precio unitario"));
+    model->setHeaderData(4, Qt::Horizontal, QObject::tr("Stock"));
+
+    ui->tablaProducto->setModel(model);
+    ui->tablaProducto->show();
+}
+
+void MainWindow::on_agregarEmpleado_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+    ui->label->setText("AGREGAR EMPLEADO");
+    // entrada -> SQL query
+
+}
+
+void MainWindow::on_editarEmpleado_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(4);
+    ui->label->setText("EDITAR EMPLEADO");
+    // entrada -> SQL query
+}
+
+void MainWindow::on_agregarProveedor_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(5);
+    ui->label->setText("AGREGAR PROVEEDOR");
+    // entrada -> SQL query
+}
+
+void MainWindow::on_editarProveedor_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(6);
+    ui->label->setText("EDITAR PROVEEDOR");
+    // entrada -> SQL query
+}
+
+void MainWindow::on_agregarProducto_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(7);
+    ui->label->setText("AGREGAR PRODUCTO");
+    // entrada -> SQL query
+}
+
+void MainWindow::on_editarProducto_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(8);
+    ui->label->setText("EDITAR PRODUCTO");
+    // entrada -> SQL query
+}
+
+void MainWindow::on_realizarVenta_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(9);
+    ui->label->setText("REALIZAR VENTA");
+    // entrada -> SQL query
+}
+
+void MainWindow::on_realizarCompra_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(10);
+    ui->label->setText("REALIZAR COMPRA");
+    // entrada -> SQL query
+}
+
+void MainWindow::on_salir_clicked()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_codigoPistola_returnPressed()
+{
+    QString codigo = ui->codigoPistola->text();
+    QString cantidad = ui->cantidad->text();
+    if (codigo == "" || cantidad == "") return;
+    int cantidadI = cantidad.toInt();
+    // validación
+    QString query = "SELECT * FROM PRODUCTO WHERE COD_BR_PD = %0";
+    model->setQuery(query.arg(codigo) , db);
+
+    QString verif = model->data(model->index(0,0)).toString();
+
+    if (verif == codigo){
+        // producto existe
+        QString nombre = model->query().value(2).toString();
+        int precio=model->query().value(3).toInt();
+        ui->label_28->setText(nombre);
+        QString espacio = QString(" ").repeated(80 - nombre.size() - cantidad.size());
+        QString linea = nombre + espacio + cantidad;
+        ui->detalleBoleta->append(linea);
+        ListaCompra.append(std::tuple<int,int,int,QString>(codigo.toInt(),cantidadI, precio,linea));
+        std::get<0>(ListaCompra[0]);
+        ui->totalBoleta->display(ui->totalBoleta->intValue() + precio*cantidadI);
+        ui->cantidad->setText(QString("1"));
+        ui->codigoPistola->clear();
+        ui->codigoPistola->setFocus();
+    }
+    else ui->label_28->setText("PRODUCTO NO EXISTE");
+
+}
+
+void MainWindow::on_cantidad_returnPressed()
+{
+    ui->codigoPistola->setFocus();
+}
+
+
+void MainWindow::on_emitirBoleta_clicked()
+{
+    // actualizar base
+    // generar PDF
+    // Imprimir
+
+    for (std::tuple<int,int,int,QString> elemento: ListaCompra){
+        QString queryCantidad = "SELECT CANTIDAD_RP FROM REG_PROVISION WHERE COD_BR_PD = %0";
+        QString query = "UPDATE REG_PROVISION SET CANTIDAD_RP = %0 WHERE COD_BR_PD = %1";
+        model->setQuery(queryCantidad.arg(std::get<0>(elemento)),db);
+        int cantidad = model->data(model->index(0,0)).toInt();
+        model->setQuery(query.arg(cantidad - std::get<1>(elemento)).arg(std::get<0>(elemento)));
+    }
+
+    ui->detalleBoleta->clear();
+    ui->totalBoleta->display(0);
+    ListaCompra.clear();
+}
+
+void MainWindow::on_codigoBorrar_returnPressed()
+{
+    //borrar elemento
+    if (ui->codigoBorrar->text() == "") return;
+    int codigo = ui->codigoBorrar->text().toInt();
+    ui->detalleBoleta->clear();
+    // volver a calular precio
+    int precioTotal = 0;
+    QVector<std::tuple<int,int,int,QString>> nuevaListaCompra;
+    for (std::tuple<int,int,int,QString> elemento: ListaCompra){
+        if (std::get<0>(elemento) != codigo){
+            nuevaListaCompra.append(elemento);
+            precioTotal+=std::get<1>(elemento)*std::get<2>(elemento);
+            ui->detalleBoleta->append(std::get<3>(elemento));
+        }
+    }
+    ui->codigoBorrar->clear();
+    ui->totalBoleta->display(precioTotal);
+    ListaCompra=nuevaListaCompra;
+}
+
+void MainWindow::on_conectar_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+
+    QString driver = ui->driver->text(),
+            server = ui->server_name->text(),
+            dataBase = ui->DDBB->text(),
+            userID = ui->userID->text(),
+            password = ui->password->text(),
+            texto = "DRIVER=%0;SERVER=%1;DATABASE=%2;UID=%3;PWD=%4";
+
+    db = QSqlDatabase::addDatabase("QODBC3");
+    db.setDatabaseName(texto.arg(driver).arg(server).arg(dataBase).arg(userID).arg(password));
+
+    bool ok = db.open();
+
+    if (ok) ui->log->setText(QString("OK"));
+    else ui->log->setText(QString(db.lastError().text()));
+
+}
+
---
+++ ProyectoSIA/mainwindow.h
@@ -0,0 +1,62 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlQueryModel>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_infoEmpleado_clicked();
+
+    void on_listaProducto_clicked();
+
+    void on_agregarEmpleado_clicked();
+
+    void on_editarEmpleado_clicked();
+
+    void on_agregarProveedor_clicked();
+
+    void on_editarProveedor_clicked();
+
+    void on_agregarProducto_clicked();
+
+    void on_editarProducto_clicked();
+
+    void on_realizarVenta_clicked();
+
+    void on_realizarCompra_clicked();
+
+    void on_codigoPistola_returnPressed();
+
+    void on_cantidad_returnPressed();
+
+    void on_salir_clicked();
+
+    void on_emitirBoleta_clicked();
+
+    void on_codigoBorrar_returnPressed();
+
+    void on_conectar_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+    QSqlDatabase db;
+    QSqlQueryModel* model;
+    QVector<std::tuple<int,int,int,QString>> ListaCompra;
+};
+
+#endif // MAINWINDOW_H
